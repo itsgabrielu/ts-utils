7dff8c3f2d6cd450bfe2b6b836220431014f5146
woodysee <woodyseesk@gmail.com>
2021-10-07 13:22:43 +0800

    Tweak call order explantions

--- tests/call-order.test.ts
+++ tests/call-order.test.ts
@@ -5,30 +5,33 @@ import { expect } from "chai";
  */
 const getOrderOfLogs = () => {
   console.log(
-    "Synchronously execute the script as though it were a function body. Run until the Call Stack is empty.",
+    "Synchronously execute the script as though it were a function body. Run until the call stack is empty.",
   );
-  console.log("Script evaluates timeout...");
+  console.log("Evaluating timeout.");
   setTimeout(function timeoutTask() {
     console.log(
       "With the script evaluated and microtasks queue empty, microtask queue will push the timeoutTask() task frame into the call stack, executing it, then leaves the call stack immediately.",
     );
-    console.log("1");
+    console.log("4 / 4");
   }, 0);
   console.log(
-    "Evaluated timeout. The event loop pushes timeoutTask() callback via the Web API into the task queue (callback queue). As script is still evaluating, timeoutTask() stays in the task queue.",
+    "Evaluated timeout. The event loop detects timeoutTask() callback, pushes it into the task queue (callback queue) via the Web API. As script is still evaluating, timeoutTask() stays in the task queue.",
   );
   console.log(
     "Evaluating instantiated promise. executor() is executed on instantiation so it is evaluated, and is pushed onto the call stack by the event loop.",
   );
   new Promise(function executor(resolve) {
     console.log("executor() is now evaluated and synchronously executed.");
-    console.log("2");
+    console.log("1 / 4");
     console.log(
-      "resolve() is evaluated as thenFn() and enters the microtasks queue. As the call stack is still evaluating the script, it stays there until the call stack is clear.",
+      "resolve() is evaluated as thenFn() and enters the microtasks queue.",
     );
-    resolve("3");
+    resolve("3 / 4");
     console.log(
-      "executor() has been evaluated and executed and is popped from the call stack.",
+      "As the call stack is still evaluating executor() and the script, thenFn() stays there until the call stack is clear.",
+    );
+    console.log(
+      "executor() has been evaluated and executed and is popped from the call stack. Script evaluation continues.",
     );
   }).then(function thenFn(value) {
     console.log(
@@ -39,11 +42,11 @@ const getOrderOfLogs = () => {
       `thenFn() has been evaluated and clears the frame from the call stack.`,
     );
   });
-  console.log("Promise statement has been evaluated.");
+  console.log("Promise instantiation has been evaluated.");
+  console.log("2 / 4");
   console.log(
-    "4 gets called next, pushed onto the call stack, executes, leaves the call stack. Script finishes evaluating, popping the evaluation frame out of the call stack, leaving the call stack empty. As there is still a task and microtask queued, the priority goes to the microtask.",
+    "Script finishes evaluating, popping the script evaluation frame out of the call stack, leaving the call stack empty. As there is still a task (timeoutTask()) and microtask (thenFn()) queued, the priority goes to the microtasks.",
   );
-  console.log("4");
 };
 
 /**
