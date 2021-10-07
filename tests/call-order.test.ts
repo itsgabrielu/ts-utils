d19ed554eeaff21c5d07f0c41e1d46617d734112
woodysee <woodyseesk@gmail.com>
2021-10-07 12:43:00 +0800

    Fix call order log descriptions

--- tests/call-order.test.ts
+++ tests/call-order.test.ts
@@ -1,50 +1,49 @@
 import { expect } from "chai";
 
-const getOrderOfLogs = (arr: string[]) => {
+/**
+ * @see https://www.jsv9000.app/
+ */
+const getOrderOfLogs = () => {
   console.log(
-    "I. Script starts evaluating. Call stack is evaluating script... no microtask can be pushed.",
+    "Synchronously execute the script as though it were a function body. Run until the Call Stack is empty.",
   );
-  console.log(
-    "II. Script evaluates timeout and pushes timeout callback via the Web API into the Task Queue.",
-  );
-  setTimeout(() => {
+  console.log("Script evaluates timeout...");
+  setTimeout(function timeoutTask() {
     console.log(
-      "X. With the script evaluated and tasks queue empty, microtask queue will push the timeout callback task frame into the call stack, executing it, and leaves the call stack immediately.",
+      "With the script evaluated and microtasks queue empty, microtask queue will push the timeoutTask() task frame into the call stack, executing it, then leaves the call stack immediately.",
     );
     console.log("1");
-    arr.push("1");
   }, 0);
   console.log(
-    "III. Script evaluated timeout. As script is still evaluating, timeout callback stays in the task queue.",
+    "Evaluated timeout. The event loop pushes timeoutTask() callback via the Web API into the task queue (callback queue). As script is still evaluating, timeoutTask() stays in the task queue.",
   );
   console.log(
-    "IV. Script evaluates promise, which is immediately instantiated, and runs the callback immediately before resolve.",
+    "Evaluating instantiated promise. executor() is executed on instantiation so it is evaluated, and is pushed onto the call stack by the event loop.",
   );
-  new Promise<"3">((resolve) => {
-    console.log(
-      "V. Task queue pushes resolve into the call stack and executes the callback.",
-    );
+  new Promise(function executor(resolve) {
+    console.log("executor() is now evaluated and synchronously executed.");
     console.log("2");
-    arr.push("2");
-    resolve("3");
-  }).then((value) => {
     console.log(
-      "VIII. With the script evaluated, the task queue executes .then callback as the call stack is clear.",
+      "resolve() is evaluated as thenFn() and enters the microtasks queue. As the call stack is still evaluating the script, it stays there until the call stack is clear.",
+    );
+    resolve("3");
+    console.log(
+      "executor() has been evaluated and executed and is popped from the call stack.",
+    );
+  }).then(function thenFn(value) {
+    console.log(
+      "With the call stack clear, thenFn() is pushed from the microtasks queue onto the call stack by the event loop to be evaluated and synchonously executed.",
     );
     console.log(value);
     console.log(
-      `IX. Task queue pushes ${value} log into the call stack, it executes, then clears the frame from the stack.`,
+      `thenFn() has been evaluated and clears the frame from the call stack.`,
     );
-    arr.push(value);
   });
+  console.log("Promise statement has been evaluated.");
   console.log(
-    "VI. Promise has evaluated the callback and evaluates the resolve callback. As the script is still being evaluated, the resolve callback cannot enter the call stack.",
-  );
-  console.log(
-    "VII. 4 gets called next, pushed onto the call stack, executes, leaves the call stack. Script finishes evaluating, popping the evaluation frame out of the call stack and opens the door for microtasks and tasks callback queue.",
+    "4 gets called next, pushed onto the call stack, executes, leaves the call stack. Script finishes evaluating, popping the evaluation frame out of the call stack, leaving the call stack empty. As there is still a task and microtask queued, the priority goes to the microtask.",
   );
   console.log("4");
-  arr.push("4");
 };
 
 /**
@@ -61,7 +60,7 @@ describe("Order of logs", () => {
     describe(`For the problem`, () => {
       it(`should give ${first} merge ${second} -> ${expected}`, () => {
         const arr: string[] = [];
-        getOrderOfLogs(arr);
+        getOrderOfLogs();
         // expect(arr).to.deep.equal(["2", "4", "3", "1"]);
         expect(2).lessThanOrEqual(2); // Fake tests
       });
